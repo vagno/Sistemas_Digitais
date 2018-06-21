8155cbcf0bd0b70f2ca8a8cd50bee83a4e3012b9
vagno <37219434+vagno@users.noreply.github.com>
2018-06-21 15:05:02 -0300

    Add files via upload

---
+++ T4-Desenhar triângulo em c/Desenho triângulo.c
@@ -0,0 +1,81 @@
+
+#include <stdio.h>
+
+
+int area(int x1, int x2, int x3, int y1, int y2, int y3){
+    int a;
+    a = ((x1*y2 + y1*x3 + x2*y3)-(x3*y2 + y3*x1 + x2*y1))/2;
+    return a;
+}
+
+int triangulo( int x1, int x2, int x3, int y1, int y2, int y3, int i, int j){
+    int t1 = area (x1, x2, i ,y1, y2, j);
+    int t2 = area (x1, x3, i ,y1, y3, j);
+    int t3 = area (x2, x3, i ,y2, y3, j);
+    int t = area (x1, x2, x3, y1, y2, y3);
+
+
+    return t1 + t2 + t3 <= t;
+}
+
+
+
+int main(){
+/*
+    int i, a;
+    int x1 , y1;
+    int x2 , y2;
+    int x3 , y3;
+    int l = 20 , c = 20; //tamanho da tela
+//    printf("tamanho da tela: ");
+
+    
+    printf("Digite os pontos A x1 do triangulo: ");
+    scanf("%d", x1);
+    printf("Digite os pontos A y1 do triangulo: ");
+        scanf("%d", y1);
+    printf("Digite os pontos B x2 do triangulo: ");
+        scanf("%d", x2);
+    printf("Digite os pontos B y2 do triangulo: ");
+        scanf("%d", y2);
+    printf("Digite os pontos C x3 do triangulo: ");
+        scanf("%d", x3);
+    printf("Digite os pontos C y3 do triangulo: ");
+        scanf("%d", y3);
+    
+    for (i=0 ; i<=l ; i++){
+        
+        a = ((x1*y2 + y1*x3 + x2*y3)- ())
+    }
+
+}*/
+
+
+int x1, y1, x2, y2, x3 , y3 ;
+x1=1;
+x2=1;
+y1=4;
+y2=1;
+x3=1;
+y3=5;
+
+
+int x = 10; //linhas por coluna
+
+for (int i=0 ; i<x; ++i){
+    for (int j =0; j<x; ++j){
+        if (triangulo(x1, x2, x3, y1, y2, y3, i, j)){
+            
+            printf("X");
+        }
+        else{
+            printf("o");
+        }
+    }
+}
+}
+
+
+
+
+
